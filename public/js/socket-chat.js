19da7e110442d419b377e5b2d9fc15f196b007f9
Carlos <1310374@utp.edu.pe>
2019-04-17 17:25:05 -0500

    Chat finalizado v1

--- public/js/socket-chat.js
+++ public/js/socket-chat.js
@@ -1,54 +1,52 @@
 var socket = io();
+
 var params = new URLSearchParams(window.location.search);
 
 if (!params.has('nombre') || !params.has('sala')) {
-    window.location = "index.html";
+    window.location = 'index.html';
     throw new Error('El nombre y sala son necesarios');
 }
 
 var usuario = {
     nombre: params.get('nombre'),
     sala: params.get('sala')
-}
+};
+
+
 
 socket.on('connect', function () {
     console.log('Conectado al servidor');
 
     socket.emit('entrarChat', usuario, function (resp) {
-        console.log('Usuarios conectados: ', resp);
+        //console.log('Usuarios conectados', resp);
+        renderizarUsuarios(resp);
+        buscarUsuario(resp);
     });
+
 });
 
 // escuchar
 socket.on('disconnect', function () {
-
     console.log('Perdimos conexión con el servidor');
-
 });
-
-
-// Enviar información
-// socket.emit('crearMensaje', {
-//     usuario: 'Fernando',
-//     mensaje: 'Hola Mundo'
-// }, function (resp) {
-//     console.log('respuesta server: ', resp);
-// });
 
 // Escuchar información
 socket.on('crearMensaje', function (mensaje) {
-    console.log('Servidor:', mensaje);
+    //console.log('Servidor:', mensaje);
+    renderizarMensajes(mensaje, false);
+    scrollBottom();
 });
 
 // Escuchar cambios de usuarios
-// Cuando un usuario entra o sale del chat
-socket.on('listaPersonas', function (personas) {
-
-    console.log(personas);
-
+// cuando un usuario entra o sale del chat
+socket.on('listaPersona', function (personas) {
+    renderizarUsuarios(personas);
+    buscarUsuario(personas);
 });
 
-// Mensaje Privados
+// Mensajes privados
 socket.on('mensajePrivado', function (mensaje) {
-    console.log('Mensaje privado: ', mensaje);
+    console.log('Mensaje Privado:', mensaje);
+    renderizarMensajes(mensaje, false);
+    scrollBottom();
 });
